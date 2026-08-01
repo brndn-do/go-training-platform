1e491289a0ca112c84dbe262d45cbf32f0e814bf
brndn-do <dobrandon05@gmail.com>
2026-08-01 15:31:37 -0500

    style(domain): use positional record syntax

--- backend/src/GoTrainingPlatform.Domain/Coordinates.cs
+++ backend/src/GoTrainingPlatform.Domain/Coordinates.cs
@@ -3,15 +3,4 @@ namespace GoTrainingPlatform.Domain;
 /// <summary>
 /// Represents the x, y coordinates of a Go Board.
 /// </summary>
-public record Coordinates
-{
-  /// <summary>
-  /// Gets the x-coordinate.
-  /// </summary>
-  public int X { get; init; }
-
-  /// <summary>
-  /// Gets the y-coordinate.
-  /// </summary>
-  public int Y { get; init; }
-}
+public record Coordinates(int X, int Y);
--- backend/src/GoTrainingPlatform.Domain/Move.cs
+++ backend/src/GoTrainingPlatform.Domain/Move.cs
@@ -3,10 +3,4 @@ namespace GoTrainingPlatform.Domain;
 /// <summary>
 /// Represents a move.
 /// </summary>
-public record Move
-{
-  /// <summary>
-  /// Gets the coordinates of the move, null if the move is a pass.
-  /// </summary>
-  public Coordinates? Coordinates { get; init; }
-}
+public record Move(Coordinates? Coordinates);
